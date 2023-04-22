081c778e5ef906608bdceb889d4320230451dd8d
BingNa Yang <bing334@gmail.com>
2023-04-22 19:40:08 -0400

    Defining The Review Model
    Create models/review.js: defining the review model
    Update models/book.js: add review property

--- models/book.js
+++ models/book.js
@@ -8,6 +8,12 @@ const BookSchema = new Schema({
     description: String,
     isbn: String,
     image: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 })
 
 module.exports = mongoose.model('Book', BookSchema)
