b649b62eb207c316eda58c69615f5b5cf6f24a7d
BingNa Yang <bing334@gmail.com>
2023-04-25 18:40:56 -0400

    Book Delete Middleware
    Update models/book.js: create middleware to delete book with all reviews

--- models/book.js
+++ models/book.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const BookSchema = new Schema({
     title: String,
@@ -14,6 +15,16 @@ const BookSchema = new Schema({
             ref: 'Review'
         }
     ]
+})
+
+BookSchema.post('findOneAndDelete', async function (doc) {
+    if(doc){
+        await Review.deleteMany({
+            _id:{
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 module.exports = mongoose.model('Book', BookSchema)
